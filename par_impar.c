60f5cbc596feab658e330b244d5af3d34834c078
Ernesto Danjiro López Sugahara <79927497+danjiro01@users.noreply.github.com>
2023-08-23 19:42:57 -0600

    Agregando primo.c
    Agregando un código en C que determina si un número es o no primo.

---
+++ par_impar.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool verificarPrimo(int num);
+
+
+int main () {
+  int num;
+  printf("Ingrese un número entero: ");
+  scanf("%d", &num);
+  if (num < 0) {
+    printf("ERROR. Ingrese un número positivo");
+    return 0;
+  }
+  if (verificarPrimo(num)) {
+      printf("El número %d es primo", num);
+  } else{
+      printf("El número %d no es primo", num);
+  }
+  return 0;
+}
+
+bool verificarPrimo (int num) {
+    int i;
+    if (num == 0 || num == 1) {
+        return false;
+    }
+    for (i = 2; i < num; i++) {
+        if (num % i == 0 && i != num) {
+            return false;
+        }
+    }
+    return true;
+}
