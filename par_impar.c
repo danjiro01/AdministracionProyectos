b957d5d65d31f4e0bd09c641488aa581a7a30a36
CelisYu25 <80054550+CelisYu25@users.noreply.github.com>
2023-08-23 20:28:43 -0600

    Update par_impar.c

--- par_impar.c
+++ par_impar.c
@@ -1,9 +1,8 @@
-/* Programa que realiza el calculo de un número primo */
+/* Programa que determina si un número es primo*/
 #include <stdio.h>
 #include <stdbool.h>
 
-bool verificarPrimo(int num);
-
+bool verificarPrimo(int num); 
 
 int main () {
   int num;
@@ -23,11 +22,11 @@ int main () {
 
 bool verificarPrimo (int num) {
     int i;
-    if (num == 0 || num == 1) {
+    if (num == 0 || num == 1) { 
         return false;
     }
-    for (i = 2; i < num; i++) {
-        if (num % i == 0 && i != num) {
+    for (i = 2; i < num; i++) { /* Se verifica que el numero ingresado sea divisible por 2 */  
+        if (num % i == 0 && i != num) { 
             return false;
         }
     }
